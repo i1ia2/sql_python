27e5dff9fa131731444da643c4bbffd1506cd007
Уваров Илья <popodoss@mail.ru>
2023-01-22 15:31:44 +0300

    Дороработка

--- main.py
+++ main.py
@@ -1,80 +1,117 @@
 import psycopg2
+def create_db(cur):
 
-def create_db(conn):
-    with conn.cursor() as cur:
         cur.execute('''
-                     CREATE TABLE info(
-                            id SERIAL PRIMARY KEY,
-                            name VARCHAR(40),
-                            surname VARCHAR(60),
-                            email VARCHAR(120),
-                            phone text);''')
-
-def creat_client(conn, id, name=None, surname=None, email=None, phones=None):
-    with conn.cursor() as cur:
-        name = "Aex"
-        surname = "pai"
-        email = "2"
-        phone = "+79995999444"
-        cur.execute(
-            f"INSERT INTO info( name, surname, email, phone) VALUES('{name}', '{surname}', '{email}',"
-            f"'{phone}');")
+                        CREATE TABLE info(
+                               PersonId SERIAL PRIMARY KEY,
+                               name VARCHAR(40),
+                               surname VARCHAR(60),
+                               email VARCHAR(120)
+                               );''')
+        cur.execute('''
+                        CREATE TABLE phone_client( 
+                            PersonId SERIAL ,
+                            Foreign key (PersonId) references info(PersonId),
+                            phone TEXT
+                            );''')
 
 
-def add_phone(conn, id, phone=None):
-    with conn.cursor() as cur:
+
+
+def creat_client(cur, name=None, surname=None, email=None, phone=None):
+
+        cur.execute("""INSERT INTO info(name, surname, email) VALUES(%s,%s,%s);""",
+                    (name, surname, email,))
+
+        cur.execute("""INSERT INTO phone_client(phone) VALUES(%s);""",
+                    (phone,))
+
+def add_phone(cur, PersonId, phone=None):
+        cur.execute("""INSERT INTO phone_client(PersonId,phone) VALUES(%s,%s);""",
+                    (PersonId, phone,))
+
+def change_client(cur, PersonId, name=None, surname=None, email=None, phone=None):
         cur.execute("""
-            UPDATE info 
-                SET phone = CONCAT(phone, '+89 96 345')
-                WHERE id = 1
-        """)
-
-def change_client(conn, id, name=None, surname=None, email=None, phones=None):
-    with conn.cursor() as cur:
+               UPDATE info
+                   SET name=%s, surname=%s, email=%s
+                   WHERE PersonId=%s
+           """, (name, surname, email, PersonId,))
         cur.execute("""
-               UPDATE info 
-                   SET name = 'pasdkol', surname = 'hopsin', email = 'leps@mail.ru', phone = '' 
-                   WHERE id = 1
-           """)
+                UPDATE phone_client
+                    SET phone=%s
+                    WHERE PersonId=%s
+            """, (PersonId, phone))
 
-def delete_client(conn):
-    with conn.cursor() as cur:
+def delete_client(cur, PersonId):
         cur.execute("""
-            DELETE FROM info WHERE ID = 1;
-        """)
+            DELETE FROM info WHERE PersonId=%s;
+        """, (PersonId,))
 
-def find_client(conn, name=None, surname=None, email=None, phone=None):
-    with conn.cursor() as cur:
+def find_client(cur, name=None, surname=None, email=None, phone=None):
         cur.execute("""
-            SELECT id, name, surname, email, phone FROM info 
-            WHERE name LIKE '%pasdkol%'
-            """)
+            SELECT name, surname, email, phone FROM info i
+            JOIN phone_client p on i.PersonId = p.PersonId
+            WHERE (name=%s) or (surname=%s) or (email=%s) or (phone=%s)
+            """, (name, surname, email, phone))
         print(cur.fetchone())
 
-#Удаление телефона
-def delete_phone(conn, id, phone=None):
-    with conn.cursor() as cur:
-        cur.execute("""
-            UPDATE info 
-                SET phone = ''
-                WHERE id = '1'
-        """)
 
-def drop_table(conn):
-    with conn.cursor() as cur:
+#Удаление телефона
+def delete_phone(cur, PersonId, phone=None):
         cur.execute("""
+            DELETE FROM phone_client
+            WHERE (personId=%s) and (phone=%s)
+        """, (PersonId, phone, ))
+
+def drop_table(cur):
+        cur.execute("""
+         DROP TABLE phone_client;
                     DROP TABLE info;
                     """)
+if __name__ == "__main__":
+    with psycopg2.connect(database="postgres", user="postgres", password="153268425Zz", host='localhost') as conn:
+            print('''Вы можете: 
+                Функция 1 создающая структуру БД (таблицы)
+                Функция 2 позволяющая добавить нового клиента
+                Функция 3 позволяющая добавить телефон для существующего клиента
+                Функция 4 позволяющая изменить данные о клиенте
+                Функция 5 позволяющая удалить телефон для существующего клиента
+                Функция 6 позволяющая удалить существующего клиента
+                Функция 7 позволяющая найти клиента по его данным (имени, фамилии, email-у или телефону)
+                Функция 8 позволяющая удалить БД (таблицы)''')
 
-with psycopg2.connect(database="postgres", user="postgres", password="", host='localhost') as conn:
-    # create_db(conn)
-    # creat_client(conn, id, name=None, surname=None, email=None, phones=None)
-    # find_client(conn, name=None, surname=None, email=None, phone=None)
-    # change_client(conn, id, name=None, surname=None, email=None, phones=None)
-    # delete_client(conn)
-    # delete_phone(conn, id, phone=None)
-    # add_phone(conn, id, phone=None)
-    # drop_table(conn)
+            comanda = int(input("Выберите функцию: "))
+            with conn.cursor() as cur:
+                if comanda == 1:
+                    create_db(cur)
+
+                elif comanda == 2:
+                    creat_client(cur, name=input("Введите имя: "), surname=input("Введите Фамилию: ")
+                                 , email=input("Введите емайл: "), phone=input("Введите телефон: "))
+
+                elif comanda == 3:
+                    add_phone(cur, PersonId=input('Введите номер клиента: '), phone=input('Введите номер: '))
+
+                elif comanda == 7:
+                    find_client(cur, name=input("Введите имя: "), surname=input("Введите Фамилию: "),
+                                  email=input("Введите емайл: "), phone=input("Введите Телефон: "))
+
+                elif comanda == 4:
+                    change_client(cur, PersonId=input("id: "), name=input("Введите имя: "),
+                                  surname=input("Введите Фамилию: "), email=input("Введите емайл: ")
+                                  , phone=input("Введите Телефон: "))
+
+                elif comanda == 6:
+                    delete_client(cur, PersonId=input("Введите id клиента"))
+
+                elif comanda == 5:
+                    delete_phone(cur, PersonId=input("Введите id клиента телефон которого хотите "
+                                                      " которого хотите удалить "),
+                                 phone=str(input("Введите телефон который хотите удалить")))
+
+                elif comanda == 8:
+                    drop_table(cur)
+
 
 conn.close()
 
